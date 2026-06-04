b0dde497e7c3fc5ba90b11bd56e11c0ed20a58ba
jagadeesvarrao-design <jagadeesvarrao@gmail.com>
2026-06-04 10:13:53 +0530

    Update API URL for production

--- frontend/script.js
+++ frontend/script.js
@@ -13,7 +13,7 @@ const sendBtn = document.getElementById('sendBtn');
 const chatWindow = document.getElementById('chatWindow');
 
 // API Base URL (FastAPI)
-const API_URL = 'http://127.0.0.1:8002';
+const API_URL = 'https://pdf-analizing-and-answering-bot.onrender.com';
 
 // Handle File Selection
 fileInput.addEventListener('change', () => {
